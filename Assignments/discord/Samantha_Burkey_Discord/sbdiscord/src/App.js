45b8dec14536dcd9bd59e34729566f30ac850ce0
Samantha Burkey <sburkey@wvup.edu>
2019-10-14 16:17:31 -0400

    Finished discord app

--- Assignments/discord/Samantha_Burkey_Discord/sbdiscord/src/App.js
+++ Assignments/discord/Samantha_Burkey_Discord/sbdiscord/src/App.js
@@ -11,7 +11,73 @@ const Header = () => {
   )
 };
 
-function TodoList() {
+function TodoList({todos, handleEdit, state, setState, deleteItem}) {
+  return (
+    <List divided relaxed>
+      {
+        todos.sort((a, b) => a.completed - b.completed).map(todo => {
+          // Lets take the date and generate a relative time that is friendly
+          const relativeTime = moment(todo.dateCreated).fromNow()
+          const labelStyle = {};
+          // We would like a specific style to use when a todo is completed for the ux
+          if (todo.completed) {
+            labelStyle.textDecoration = 'line-through';
+          }
+
+          // Since we now keep track of the todo the user wants to edit, we can handle this 
+          // specifically and render out a custom input option inline
+          if (todo.id === state.currentEditableTodoId) {
+            return (
+              <List.Item>
+                <List.Content style={{
+                  display: 'inline-block',
+                  paddingLeft: '20px'
+                }}>
+                  <List.Header as='a' style={labelStyle}>
+                    <Input value={state.editValue} onChange={(event, { value }) => {
+                      // Every time the user types we need to update our `editValue` so that
+                      // we have a safe way to edit a todo in an immutable fashion
+                      setState({
+                        // Spread the current state so we do not lose any values
+                        ...state,
+                        // Update our temporary label holder while the user is working
+                        editValue: value
+                      })
+                    }} />
+                    <Button primary onClick={handleEdit}>Save</Button>
+                  </List.Header>
+                </List.Content>
+              </List.Item>
+            )
+          }
+
+          return (
+            <List.Item>
+              <List.Content style={{
+                display: 'inline-block',
+                paddingLeft: '20px'
+              }}>
+                <List.Header as='a' style={labelStyle} onClick={() => {
+                  if (todo.completed) return;
+                  setState({
+                    currentEditableTodoId: todo.id,
+                    editValue: todo.label
+                  })
+                }}>{todo.label}</List.Header>
+                {!todo.completed && <List.Description as='a' style={{
+                  paddingBottom: '10px'
+                }}>{relativeTime}</List.Description>}
+                <Button primary onClick={() => deleteItem(todo.id)}>Delete</Button>
+              </List.Content>
+            </List.Item>
+          );
+        })
+      }
+    </List>
+  );
+}
+
+function App() {
   const [state, setState] = useState({
     // The id of the todo that the user is trying to edit
     currentEditableTodoId: null,
@@ -19,6 +85,7 @@ function TodoList() {
     // It will be used to update the todo label once they save their changes
     editValue: null,
   });
+  const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -58,101 +125,41 @@ function TodoList() {
     setTodos(updatedTodos);
   }
 
-  /*const addTextItem = () => {
-    const handleAdd = todos.map(tofo => {
-      moment(new Date());
-      
-    })
+  const addTextItem = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: new Date(),
+      label: inputText,
+      dateCreated: moment(new Date().fromNow),
+      priority: "low"
+    };
+    setTodos([...todos, newTodo]);
+
+    setInputText("");
   }
 
-  const deleteItem = () => {
-    const handleDelete = todos.map(todo => {
-      event.preventDefault();
-      this.setState({
+  const deleteItem = (id) => {
+    const newTodos = todos.filter(x => x.id != id);
+    setTodos(newTodos);
+  }
 
-      })
-    })
-  }*/
-
-  return (
-    <List divided relaxed>
-      {
-        todos.sort((a, b) => a.completed - b.completed).map(todo => {
-          // Lets take the date and generate a relative time that is friendly
-          const relativeTime = moment(todo.dateCreated).fromNow()
-          const labelStyle = {};
-          // We would like a specific style to use when a todo is completed for the ux
-          if (todo.completed) {
-            labelStyle.textDecoration = 'line-through';
-          }
-
-          // Since we now keep track of the todo the user wants to edit, we can handle this 
-          // specifically and render out a custom input option inline
-          if (todo.id === state.currentEditableTodoId) {
-            return (
-              <List.Item>
-                <List.Content style={{
-                  display: 'inline-block',
-                  paddingLeft: '20px'
-                }}>
-                  <List.Header as='a' style={labelStyle}>
-                    <Input value={state.editValue} onChange={(event, { value }) => {
-                      // Every time the user types we need to update our `editValue` so that
-                      // we have a safe way to edit a todo in an immutable fashion
-                      setState({
-                        // Spread the current state so we do not lose any values
-                        ...state,
-                        // Update our temporary label holder while the user is working
-                        editValue: value
-                      })
-                    }} />
-                    <Button primary onClick={handleEdit}>Save</Button>
-                  </List.Header>
-                  <Button /*primary onClick={handleDelete}*/>Delete</Button>
-                </List.Content>
-              </List.Item>
-            )
-          }
-
-          return (
-            <List.Item>
-              <List.Content style={{
-                display: 'inline-block',
-                paddingLeft: '20px'
-              }}>
-                <List.Header as='a' style={labelStyle} onClick={() => {
-                  if (todo.completed) return;
-                  setState({
-                    currentEditableTodoId: todo.id,
-                    editValue: todo.label
-                  })
-                }}>{todo.label}</List.Header>
-                {!todo.completed && <List.Description as='a' style={{
-                  paddingBottom: '10px'
-                }}>{relativeTime}</List.Description>}
-                <Button /*primary onClick={handleDelete}*/>Delete</Button>
-              </List.Content>
-            </List.Item>
-          );
-        })
-      }
-    </List>
-  );
-}
-
-function App() {
   return (
     <Container>
       <Header />
 
       <Container fluid textAlign="center">
-        <Input 
-          type="text" 
-          placeholder="Enter text..." />
-        <Button class="ui button" /*primary onClick={handleAdd}*/>Enter</Button>
+        <form onSubmit={addTextItem}>
+          <Input 
+            type="text" 
+            placeholder="Enter text..." 
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            />
+          <Button class="ui button" primary type="submit">Enter</Button>
+        </form>
       </Container>
 
-      <TodoList />
+      <TodoList handleEdit={handleEdit} todos={todos} state={state} setState={setState} deleteItem={deleteItem}/>
 
     </Container>
   );
