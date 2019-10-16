e80683c8c92b9d61c0148ba5d8cf017df6efa886
Jennifer Mackey <jennym1211@hotmail.com>
2019-10-16 19:48:51 -0400

    some functionality whoohoo

---
+++ Assignments/discord/Jennifer/chatapp/src/messagedata.js
@@ -0,0 +1,28 @@
+export const general = [
+  {
+    senderId: "jennym1211",
+    timestamp: "Today at 12:00 AM",
+    text: "Welcome to react-cord!"
+  },
+  {
+    senderId: "afreeland",
+    timestamp: "Now",
+    text: "Don't forget to turn in your assignments!"
+  },
+
+  {
+    senderId: "johndoe",
+    timestamp: "Now",
+    text: "I wish I understood react better!"
+  }
+];
+
+export const resources = [
+    {
+      sender: "afreeland",
+      timestamp: "Today at 12:00 AM",
+      text:
+        "Click on this link to learn about react hooks -> https://reactjs.org/docs/hooks-intro.html"
+    }
+  ];
+  
